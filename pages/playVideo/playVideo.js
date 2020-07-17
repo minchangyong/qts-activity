a67f5e4e8527b219160514aac6247fc54a096492
biaodi <guimashusheng@126.com>
2020-07-17 15:06:47 +0800

    第一页和第二页完成

---
+++ pages/playVideo/playVideo.js
@@ -0,0 +1,61 @@
+// pages/playVideo/playVideo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    showStatus: 0,    // 操作按钮的值
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {},
+
+  onReady() {
+    this.videoContext = wx.createVideoContext('myVideo')
+  },
+
+  skipPage: function() {
+
+  },
+
+  /**
+   * 切换按钮状态
+   */
+  skipStatus() {
+    this.setData({
+      showStatus: 3
+    })
+    this.videoContext.pause()
+    setTimeout(() => {
+      this.setData({
+        showStatus: -1,
+      })
+      this.videoContext.play()
+    }, 2000)
+  },
+
+  /**
+   * 视频播放结束
+   */
+  handleEnd() {
+    this.setData({
+      showStatus: 2
+    })
+  },
+
+  /**
+   * 播放进度更新
+   */
+  bindtimeupdate(e) {
+    const { currentTime } = e.detail
+    if (currentTime > 3 && this.data.showStatus === 0 && !this.isSkiped) {
+      this.isSkiped = true
+      this.setData({
+        showStatus: 1
+      })
+    }
+  }
+})
